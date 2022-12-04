5e43da63833b441eac908e6c2d972b170fc34141
julienh62 <78169249+julienh62@users.noreply.github.com>
2022-12-04 22:13:58 +0100

    registration and login

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -48,7 +48,11 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        
+          return new RedirectResponse($this->urlGenerator->generate('app_home'));
+
+
     }
 
     protected function getLoginUrl(Request $request): string
